ef2b820ba89e5052f7e13391d5726994a829ebaa
Sergiu <condratiuc.sergiu@gmail.com>
2026-01-20 17:50:14 +0100

    1

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const script = document.createElement("script");
     script.type = "module";
     script.src = "https://agent.d-id.com/v2/index.js";
-    script.setAttribute("data-client-key", "CLIENT_KEY_DE_LA_BASTIAN");
+    script.setAttribute("data-client-key", "c3RhbmR3aXRodXNpc3JhZWxAZ21haWwuY29t:llldq-gXzqRpkoTlixn7_");
     script.setAttribute("data-agent-id", "v2_agt_bRJbyftV");
     script.setAttribute("data-name", "did-agent");
     document.body.appendChild(script);
